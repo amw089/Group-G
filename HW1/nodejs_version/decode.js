f9e1c3de96c0f303664024e851ca4cd8813fe80f
ricardoaranaga <65545723+ricardoaranaga@users.noreply.github.com>
2020-09-25 09:37:15 -0500

    Add files via upload

--- HW1/nodejs_version/decode.js
+++ HW1/nodejs_version/decode.js
@@ -1,39 +1,41 @@
-//hashtables
-const hash = (key, size) => {
-  let hashedKey = 0
-  for (let i = 0; i < key.length; i++) {
-    hashedKey += key.charCodeAt(i)
-  }
-  return hashedKey % size
+// Splits a given file into smaller subfiles by line number
+var infileName = 'dictionary.txt';
+var fileCount = 1;
+var count = 0;
+var fs = require('fs');
+var outStream;
+var outfileName = infileName + '.' + fileCount;
+newWriteStream();
+var inStream = fs.createReadStream(infileName);
+
+var lineReader = require('readline').createInterface({
+    input: inStream
+});
+
+function newWriteStream(){
+    outfileName = infileName + '.' + fileCount;
+    outStream = fs.createWriteStream(outfileName);
+    count = 0;
 }
 
-class HashTable {
-  constructor() {
-    this.size = 101
-    this.tables = Array(this.size) 
-
-    let i = 0;
-    while (i < this.tables.length) {
-      this.tables[i] = new Map()
-      i++;
+lineReader.on('line', function(line) {
+    count++;
+    outStream.write(line + '\n');
+    if (count >= 7600) {
+        fileCount++;
+        outStream.end();
+        newWriteStream();
     }
-  }
+});
 
-  insert(key, value) {
-    let idx = hash(key, this.size) 
-    this.tables[idx].set(key, value)
-  }
+lineReader.on('close', function() {
+    if (count > 0) {
+    }
+    inStream.close();
+    outStream.end();
+    console.log('Done');
+});
 
-  search(key) {
-    let idx = hash(key, this.size)
-    return this.tables[idx].get(key)
-  }
-
-  size() {
-    return this.tables.size
-  }
-
-}
 
 //uuidv5
 var crypto = require('crypto');
@@ -42,46 +44,46 @@ function uuidV5(namespace, name) {
   var hexNm = namespace.replace(/[{}\-]/g, '');
   var bytesNm = new Buffer(hexNm, 'hex');
   var bytesName = new Buffer(name, 'utf8');
-  var hashi = crypto.createHash('sha1')
+  var hash = crypto.createHash('sha1')
       .update(bytesNm).update(bytesName)
       .digest('hex');
-  return hash5(hashi);
+  return hash.substr(0,8) + '-' +
+      hash.substr(8,4) + '-' +
+      (5 + hash.substr(13,3)) + '-' +
+      ((parseInt(hash.substr(16, 2), 16) | 0x80) & 0xBF).toString(16) + hash.substr(18,2) + '-' +
+      hash.substr(20,12);
 }
 
-function hash5(hasho) {
-  return hasho.substr(0,8) + '-' +
-      hasho.substr(8,4) + '-' +
-      (5 + hasho.substr(13,3)) + '-' +
-      ((parseInt(hasho.substr(16, 2), 16) | 0x80) & 0xBF).toString(16) + hasho.substr(18,2) + '-' +
-      hasho.substr(20,12);
+const pass_hashTable = new Map();
+const uuidV5_hashTable = new Map();
+
+function populate() {
+	var cntr = 1
+	while(cntr<fileCount) {
+		var lineReader = require('readline').createInterface({
+		input: require('fs').createReadStream('dictionary.txt.'+cntr)
+		});
+
+		lineReader.on('line', function (line) {
+		passhash = require("crypto").createHash("sha256").update(line).digest("hex").toUpperCase();
+		pass_hashTable.set(passhash,line)
+		});
+
+		cntr++	
+	}
+	
+	var namespaceString = "d9b2d63d-a233-4123-847a-76838bf2413a";
+
+	var lineReader = require('readline').createInterface({
+	input: require('fs').createReadStream('names.txt')
+	});
+
+	lineReader.on('line', function (line) {
+
+	uuidhash = uuidV5(namespaceString,line);
+	uuidV5_hashTable.set(uuidhash,line);
+	});;
 }
-
-var namespaceString = "d9b2d63d-a233-4123-847a-76838bf2413a";
-const uuidV5_hashTable = new HashTable();
-
-var lineReader = require('readline').createInterface({
-input: require('fs').createReadStream('names.txt')
-});
-
-
-lineReader.on('line', function (line) {
-
-uuidhash = uuidV5(namespaceString,line);
-uuidV5_hashTable.insert(uuidhash,line);
-
-});;
-
-const pass_hashTable = new HashTable();
-var lineReader = require('readline').createInterface({
-input: require('fs').createReadStream('dictionary.txt')
-});
-
-lineReader.on('line', function (line) {
-
-passhash = require("crypto").createHash("sha256").update(line).digest("hex").toUpperCase();
-pass_hashTable.insert(passhash,line);
-
-});
 
 //timestamp hash
 function timeStampHash(timestamp) {
@@ -91,27 +93,36 @@ function timeStampHash(timestamp) {
   return moment.tz(timestamp*1000, timezone).format();
 }
 
+function io_dump() {
+	var file = process.argv[2];
+
+	var lineReader = require('readline').createInterface({
+	input: require('fs').createReadStream(file)
+	});
+
+	lineReader.on('line', function (line) {
+	var stringArray = line.split(",");
+	var linePrint = "";
+
+	if(stringArray[0] == "username") {
+		linePrint = "username,password,last_access";
+	}
+	else {
+		linePrint = "" + uuidV5_hashTable.get(stringArray[0]) +","+ pass_hashTable.get(stringArray[1]) +","+ timeStampHash(stringArray[2]);
+	}
+	
+		console.log(linePrint);	
+	});
+}
+
+
 // Start of program after hastables creation
+
+setTimeout(populate,400)
 
 if (process.argv[2] == null) {
 	console.log("Usage "+ process.argv[1] +"<dump_file.csv>");
 	process.exit(1);
 }
+setTimeout(io_dump,500)
 
-var file = process.argv[2];
-
-var lineReader = require('readline').createInterface({
-input: require('fs').createReadStream(file)
-});
-
-lineReader.on('line', function (line) {
-	var stringArray = line.split(",");
-	var linePrint = "";
-	if(stringArray[0] == "username") {
-		linePrint = "username,password,last_access";
-	}
-	else {
-		linePrint = "" + uuidV5_hashTable.search(stringArray[0]) +","+ pass_hashTable.search(stringArray[1]) +","+ timeStampHash(stringArray[2]);
-	}
-	console.log(linePrint);	
-});
