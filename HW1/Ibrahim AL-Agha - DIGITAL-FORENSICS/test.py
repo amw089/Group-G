ee3a440ff5b3a3f1cfa11f688751e746df0f02b9
Ricardo Aranaga <raa010@email.latech.edu>
2020-09-29 08:40:21 -0500

    Errasing unnecessary files and folder

--- HW1/Ibrahim AL-Agha - DIGITAL-FORENSICS/test.py
+++
@@ -1,3 +0,0 @@
-#!bin/bash/env python3
-
-print ("hello world")
--- HW1/nodejs_version/unixtimestamp.js
+++
@@ -1,9 +0,0 @@
-var moment = require('moment-timezone');
-
-var timezone = "America/Regina";
-
-if (process.argv[2] != null) {
-	var timestamp = process.argv[2];
-	process.stdout.write(moment.tz(timestamp*1000, timezone).format());
-}
-
