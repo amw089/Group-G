c530a86376ffaa01c6f23a23d9bb0c8aafa3006d
Chaoqun <chaoqun5583@gmail.com>
2020-09-30 18:35:35 -0500

    Yu-HW1

--- HW1/chaoqun.py
+++
@@ -1,8 +0,0 @@
-import csv
-from datetime import datetime
-#read csv file
-with open('database_dump.csv','rt',encoding='utf8')as csvfile:
-    reader = csv.DictReader(csvfile)
-    username = ([row['username'] for row in reader])
-for i in len(username):
-    username = 
